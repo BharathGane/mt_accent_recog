ddfd7f711c319183559f961518786ae93be88d81
bharathgane <bharath41597@gmail.com>
2019-11-11 12:02:27 +0530

    size changed

--- solver.py
+++ solver.py
@@ -95,9 +95,9 @@ def train():
 
             # print statistics
             running_loss += loss.item()
-            if i % 1000  == 999:    # print every 2000 mini-batches
+            if i % 500  == 499:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 1000))
+                      (epoch + 1, i + 1, running_loss / 499))
                 running_loss = 0.0
             gc.collect()
     torch.save(model.state_dict(), "./model4.pt")
