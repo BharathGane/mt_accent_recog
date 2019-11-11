543fa36f6f1bccde80d16301167e3f9df968391f
bharathgane <bharath41597@gmail.com>
2019-11-11 11:59:06 +0530

    size changed

--- solver.py
+++ solver.py
@@ -46,7 +46,7 @@ def data_loader(value):
                     yield (torch.tensor(np.tile(k,(1,1,1)),dtype = torch.float).cuda(),torch.tensor(np.tile(np.asarray(labels.index(i)),(1)),dtype = torch.long).cuda())
     elif value == "test":
         for i in labels:
-            source = os.path.join("./combined_wav_files",label_file_name[i][]+".wav")
+            source = os.path.join("./combined_wav_files",label_file_name[i][3]+".wav")
             # data_array = utils.read_audio_file_data(os.path.join("./combined_wav_files",label_file_name[i][3]+".wav"))
             for k in utils.read_audio_file_data_chunks(source,chunk_freq,number_of_chunks):
             # for k in range(0,traning_time_in_sec/time_each_chunk):
