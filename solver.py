0918592e304eeda80e3741a82c464ecd24174744
bharathgane <bharath41597@gmail.com>
2019-11-13 21:05:43 +0530

    preprocess

--- solver.py
+++ solver.py
@@ -8,7 +8,6 @@ from torch import nn
 import os
 import numpy as np
 import pickle
-data = pickle.load(file)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
 print(device)
@@ -39,22 +38,21 @@ def data_loader(value):
     # 1/43 second each 
     number_of_samples = 33000
     number_of_samples_in_each_chunk = 1
-    number_of_chunks_ = number_of_samples/number_of_samples_in_each_chunk
+    number_of_chunks = number_of_samples/number_of_samples_in_each_chunk
     # print freq,chunk_freq,time_each_chunk,traning_time_in_sec,number_of_chunks
     if value == "train":
         for iterator in range(number_of_chunks):
             for i in labels:
                 file_indexes = range(len(label_file_name[i])-1)
                 for j in file_indexes:
-                    # print iterator,i,j
-                    source = os.path.join("./combined_wav_files",label_file_name[i][j]+".wav")
-                    for k in utils.read_audio_file_data_pickle(source,iterator,chunk_freq,number_of_chunks):
+                    source = os.path.join("./final_features",label_file_name[i][j]+".pkl")
+                    for k in utils.read_audio_file_data_pickle(source,iterator,number_of_samples_in_each_chunk):
                         gc.collect()
                         yield (torch.tensor(np.tile(k,(1,1,1)),dtype = torch.float).cuda(),torch.tensor(np.tile(np.asarray(labels.index(i)),(1)),dtype = torch.long).cuda())
     elif value == "test":
         for i in labels:
             for j in [0,1,2,3]:
-                source = os.path.join("./combined_wav_files",label_file_name[i][j]+".wav")
+                    source = os.path.join("./final_features",label_file_name[i][j]+".pkl")
                 # data_array = utils.read_audio_file_data(os.path.join("./combined_wav_files",label_file_name[i][3]+".wav"))
                 for k in utils.read_audio_file_data_pickle_test(source,chunk_freq,number_of_chunks):
                 # for k in range(0,traning_time_in_sec/time_each_chunk):
