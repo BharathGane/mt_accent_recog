6ec4558bb60a921539d7efb0c9c083133b88e61b
bharathgane <bharath41597@gmail.com>
2019-11-15 03:54:27 +0530

    assignment 1 and 2

--- solver.py
+++ solver.py
@@ -30,7 +30,7 @@ label_file_name = {'Chinese': ['TXHC', 'BWC', 'LXC', 'NCC'],
                 'Arabic': ['YBAA', 'SKA', 'ZHAA', 'ABA']}
 labels = ["Chinese","Vietnamese","Hindi","Spanish","Korean","Arabic"]
 labels_id = {}
-labels_index = map(float(range(6)))
+labels_index = map(float,(range(6)))
 for i in range(len(labels)):
     labels_id[labels[i]] = list(0 for i in range(6))
     labels_id[labels[i]][i] = 1
