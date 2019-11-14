9266923e1573c00b36789c3eec7c01dff6f026d7
bharathgane <bharath41597@gmail.com>
2019-11-14 21:47:20 +0530

    pkl_files for smaller sample sizes

--- preprocess.py
+++ preprocess.py
@@ -10,7 +10,7 @@ def reduce_size(source):
 			final = []
 			with open(os.path.join(root,i),"rb") as file_:
 				data = pickle.load(file_)
-			pickle_out = open(os.path.join("./final_features3",i),"wb")
+			pickle_out = open(os.path.join("./pkl_mini",i),"wb")
 			pickle.dump(data[0:500*44100], pickle_out)
 			pickle_out.close()
 			gc.collect()
