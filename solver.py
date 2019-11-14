e52a6d841eb8347040555dc5036c024266d499d7
bharathgane <bharath41597@gmail.com>
2019-11-14 19:50:56 +0530

    print statements

--- solver.py
+++ solver.py
@@ -77,8 +77,8 @@ def test():
     return class_total,class_correct
 
 def train():
-    # model.load_state_dict(torch.load('./model.pt'))
-    # model.eval()
+    model.load_state_dict(torch.load('./model.pt'))
+    model.eval()
     for epoch in range(10):  # loop over the dataset multiple times
         running_loss = 0.0
         print "traning started for ",str(epoch),"epoch"
