2e2b9ad706d1e19af4c3b8769ef529952afc82dc
bharathgane <bharath41597@gmail.com>
2019-11-15 12:16:43 +0530

    not loading from prev stored model

--- solver.py
+++ solver.py
@@ -99,8 +99,8 @@ def test():
     return class_total,class_correct
 
 def train():
-    model.load_state_dict(torch.load('./final.pt'))
-    model.eval()
+    # model.load_state_dict(torch.load('./final.pt'))
+    # model.eval()
     for epoch in range(1000):  # loop over the dataset multiple times
         running_loss = 0.0
         print "traning started for ",str(epoch),"epoch"
