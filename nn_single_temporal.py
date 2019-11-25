48801f8e89de053c9b28e3ebd26698b4e3b6f7b1
raamkashyap <ramkashyap26@gmail.com>
2019-11-25 17:46:58 +0530

    added dropout and checking for kernel size 51

--- nn_single_temporal.py
+++ nn_single_temporal.py
@@ -105,7 +105,7 @@ class MyNet(nn.Module):
         self.fc2 = nn.Linear(1280, 256)
         self.fc3 = nn.Linear(256, 6)
 
-        # self.dropout = nn.Dropout(p=0.5)
+        self.dropout = nn.Dropout(p=0.5)
         self.relu = nn.ReLU()
         
     def forward(self, x):
