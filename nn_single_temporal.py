ca806b71cec05cae1b17dfa6e7b650bbe6a244c7
raamkashyap <ramkashyap26@gmail.com>
2019-11-25 14:44:14 +0530

    checking with kernel size 11 and learning rate e5

--- nn_single_temporal.py
+++ nn_single_temporal.py
@@ -163,7 +163,7 @@ class MyNet(nn.Module):
         # x12 = torch.unsqueeze(x12, 1)
 
 
-        h = x1
+        h =  x1
         
         ##############  multiFeature formed above  ##############################
         
