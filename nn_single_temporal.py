1612d6494ae245b0d850034f91b624383420633e
raamkashyap <ramkashyap26@gmail.com>
2019-11-28 00:30:56 +0530

    added dropout and checking for kernel size 501 without weight decays

--- nn_single_temporal.py
+++ nn_single_temporal.py
@@ -116,8 +116,8 @@ class MyNet(nn.Module):
         # x4 = self.relu(self.bn1_branch4(self.layer1_branch4(x)))
         # x5 = self.relu(self.bn1_branch5(self.layer1_branch5(x)))
         # x6 = self.relu(self.bn1_branch6(self.layer1_branch6(x)))
-        x7 = self.relu(self.bn1_branch7(self.layer1_branch7(x)))
-        # x8 = self.relu(self.bn1_branch8(self.layer1_branch8(x)))
+        # x7 = self.relu(self.bn1_branch7(self.layer1_branch7(x)))
+        x8 = self.relu(self.bn1_branch8(self.layer1_branch8(x)))
         # x9 = self.relu(self.bn1_branch9(self.layer1_branch9(x)))
         # x10 = self.relu(self.bn1_branch10(self.layer1_branch10(x)))
         # x11 = self.relu(self.bn1_branch11(self.layer1_branch11(x)))
@@ -129,8 +129,8 @@ class MyNet(nn.Module):
         # x4 = self.relu(self.bn2_branch4(self.layer2_branch4(x4)))
         # x5 = self.relu(self.bn2_branch5(self.layer2_branch5(x5)))
         # x6 = self.relu(self.bn2_branch6(self.layer2_branch6(x6)))
-        x7 = self.relu(self.bn2_branch7(self.layer2_branch7(x7)))
-        # x8 = self.relu(self.bn2_branch8(self.layer2_branch8(x8)))
+        # x7 = self.relu(self.bn2_branch7(self.layer2_branch7(x7)))
+        x8 = self.relu(self.bn2_branch8(self.layer2_branch8(x8)))
         # x9 = self.relu(self.bn2_branch9(self.layer2_branch9(x9)))
         # x10 = self.relu(self.bn2_branch10(self.layer2_branch10(x10)))
         # x11 = self.relu(self.bn2_branch11(self.layer2_branch11(x11)))
@@ -142,8 +142,8 @@ class MyNet(nn.Module):
         # x4 = self.pool2_branch4(x4)
         # x5 = self.pool2_branch5(x5)
         # x6 = self.pool2_branch6(x6)
-        x7 = self.pool2_branch7(x7)
-        # x8 = self.pool2_branch8(x8)
+        # x7 = self.pool2_branch7(x7)
+        x8 = self.pool2_branch8(x8)
         # x9 = self.pool2_branch9(x9)
         # x10 = self.pool2_branch10(x10)  
         # x11 = self.pool2_branch11(x11)  
@@ -155,15 +155,15 @@ class MyNet(nn.Module):
         # x4 = torch.unsqueeze(x4, 1)  
         # x5 = torch.unsqueeze(x5, 1)
         # x6 = torch.unsqueeze(x6, 1)
-        x7 = torch.unsqueeze(x7, 1)
-        # x8 = torch.unsqueeze(x8, 1)
+        # x7 = torch.unsqueeze(x7, 1)
+        x8 = torch.unsqueeze(x8, 1)
         # x9 = torch.unsqueeze(x9, 1)
         # x10 = torch.unsqueeze(x10, 1)
         # x11 = torch.unsqueeze(x11, 1)
         # x12 = torch.unsqueeze(x12, 1)
 
 
-        h =  x7
+        h =  x8
         
         ##############  multiFeature formed above  ##############################
         
